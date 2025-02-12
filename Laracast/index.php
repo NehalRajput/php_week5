7a917ae28d6bb91fb8cabbbecc8af198de9e45f5
NehalRajput <nehal17.ast@gmail.com>
2025-02-12 10:54:39 +0530

    learnt about the php partials

--- Laracast/index.php
+++ Laracast/index.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laracast page_1</title>
-</head>
-<body>
+<?php
 
-    <h1>
-        <?php 
-           echo "Hello world";
-          
-         ?>
-    </h1>
-    
-</body>
-</html>
+ $heading = "Home";
+ 
+ require "views/index.view.php";
+ 
+?>
---
+++ Laracast/views/index.view.php
@@ -0,0 +1,22 @@
+<?php
+      require "views/partial/head.php";  
+    ?>
+<?php
+      require "views/partial/nav.php";  
+    ?>
+    <?php
+      require "views/partial/banner.php";  
+    ?>
+ 
+
+
+
+ 
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p>Hello Welcome to the page</p>
+    </div>
+  </main>
+  <?php
+      require "views/partial/footer.php";  
+    ?>
