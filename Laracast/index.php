16128b5482857ce669392adaf87c6f4d75f4fb02
NehalRajput <nehal17.ast@gmail.com>
2025-02-13 15:54:37 +0530

    Extract a php database class

--- Laracast/index.php
+++ Laracast/index.php
@@ -1,26 +1,10 @@
-
 <?php
-  require 'function.php';
 
-//   require 'router.php';
+require 'function.php';
+//require 'router.php';
+require 'Database.php';
 
-  //connect to our mysqll database.
-  
-  $dsn = "mysql:host=localhost;port=3306;dbname=demo;user=root;charset=utf8mb4";
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-// Tip: This should be wrapped in a try-catch. We'll learn how, soon.
-$pdo = new PDO($dsn);
-
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post['title'] . "</li>";
-}
-
-
-
-
-?>
+dd($posts);
