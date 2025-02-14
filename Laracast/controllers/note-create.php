7968ec74087f2be52148ab77cd5ea50241f3d8fb
NehalRajput <nehal17.ast@gmail.com>
2025-02-14 17:53:38 +0530

    Intro to Form Validation

--- Laracast/controllers/note-create.php
+++ Laracast/controllers/note-create.php
@@ -5,18 +5,23 @@ $db = new Database($config['database']);
 
 $heading = 'create note';
 
-
-if($_SERVER['REQUEST_METHOD']==='POST')
+$errors = []; 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
 {
-   // dd("you submit the form");
-   $db->query('INSERT INTO notes(body , user_id)VALUES(:body, :user_id)',[
-     
-      'body' =>$_POST['body'],
-      'user_id'=>1
+    if (strlen($_POST['body']) === 0) 
+    {
+        $errors['body'] = 'A body is required';
+    }
+    if (strlen($_POST['body'])>1000) {
+        $errors['body'] = 'A body can not be more than 1000 characters';
+    }
 
-   ]);
-
-
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
