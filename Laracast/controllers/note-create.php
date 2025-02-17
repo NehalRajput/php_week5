b60d1a07d140ebf25827639711aa3d4715585ad1
NehalRajput <nehal17.ast@gmail.com>
2025-02-17 10:19:31 +0530

    a Simple Validator Class

--- Laracast/controllers/note-create.php
+++ Laracast/controllers/note-create.php
@@ -1,21 +1,29 @@
 <?php
 
+
+require 'validator.php';
+
 $config = require('config.php');
 $db = new Database($config['database']);
 
-$heading = 'create note';
+$heading = 'Create note';
+
+//dd(validator::email());
+
+
+
 
 $errors = []; 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') 
 {
-    if (strlen($_POST['body']) === 0) 
-    {
-        $errors['body'] = 'A body is required';
-    }
-    if (strlen($_POST['body'])>1000) {
-        $errors['body'] = 'A body can not be more than 1000 characters';
-    }
 
+   
+   
+    if (!$validator::string($_POST['body'],1,1000)) 
+    {
+        $errors['body'] = 'A body of no more than 1,000 characters is required';
+    }
+ 
     if (empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
             'body' => $_POST['body'],
